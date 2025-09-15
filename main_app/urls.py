ab45726ae87bc261a0ab0d202625c1a5f7ca5e2c
Tia Piol <tiarox02@yahoo.com>
2025-09-15 09:39:47 -0700

    created home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('wizards/', views.wizard_index, name='wizard-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+class Wizard:
+    def __init__(self, name, house, age):
+        self.name = name
+        self.house = house
+        self.age = age
+
+# Create a list of Cat instances
+wizards = [
+    Wizard('Harry', 'gryffindor', 17),
+    Wizard('Hermione', 'gryffindor', 17),
+    Wizard('Ron', 'gryffindor', 17),
+]
+
+def wizard_index(request):
+    return render(request, 'wizards/index.html', {'wizards': wizards})
